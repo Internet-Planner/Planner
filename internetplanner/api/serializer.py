9f0ae1180fafc55424ceeb851f4a4e242c36f1d7
Ryan <ryan@MacBook-Pro-de-admin.local>
2023-07-11 09:23:27 +0200

    Added Class on serializer

--- internetplanner/api/serializer.py
+++ internetplanner/api/serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Food
+from .models import Food, Events, Video
  
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model=Food
         fields=('name','description')
+class EventsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Events
+        fields=('live','title', 'duration', 'id_video')
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Video
+        fields=('id_event','link')
